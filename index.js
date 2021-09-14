2c9c37322967473f66775e88ff1ba7967e423cb4
Dariy Pavlyk <dariypavlyk@gmail.com>
2021-09-14 13:03:41 +0300

    trials

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// import {
-//   authorization,
-//   createVisit,
-//   getCards,
-//   deleteCard,
-//   editCard,
-// } from "./js/API";
-// const email = "sometrialemail24@gmai.com";
-// const password = "trialpass231";
+import {
+  authorization,
+  createVisit,
+  getCards,
+  deleteCard,
+  editCard,
+} from "./js/API.js";
+import tokenVerification from "./js/TokenVerification.js";
+const email = "sorer4f@gmail.com";
+const password = "dfddg33445";
 
-// authorization(email, password).then(async (res) => {
-//   console.log(res);
-// });
-
-fetch("https://ajax.test-danit.com/api/v2/cards/login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    email: "youasf34r@email.com",
-    password: "passworsfg435d",
-  }),
-})
-  .then((response) => response.text())
-  .then((token) => console.log(token));
+const response = authorization(email, password).then((res) => {
+  console.log(res);
+});
+console.log(localStorage.getItem("token"));
+tokenVerification();
--- js/tokenVerification.js
+++ js/tokenVerification.js
@@ -1,6 +1,7 @@
-export function tokenVerification() {
+export default function tokenVerification() {
   if (localStorage.getItem("token")) {
     //render site
+    console.log("trial");
   } else {
     // render login form
   }
