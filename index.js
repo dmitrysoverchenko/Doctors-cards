80efa6e95e7405054c542b2ff0bb8e7003727a47
Dariy Pavlyk <dariypavlyk@gmail.com>
2021-09-15 13:26:15 +0300

    index cleaned

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 import { modal } from "./js/modal.js";
-// import { createVisit } from "./js/createVisit.js";
 import { loginForm } from "./js/login.js";
 
 const enterBtn = document.querySelector("#enter-btn");
-// const createVisitBtn = document.querySelector(".create-visit-btn");
 
-// createVisitBtn.addEventListener("click", function () {
-//   document.body.prepend(modal.render("Create visit", createVisit.render()));
-//   modal.show();
-// });
 enterBtn.addEventListener("click", function () {
   document.body.prepend(modal.render("Registration", loginForm.render()));
   modal.show();
